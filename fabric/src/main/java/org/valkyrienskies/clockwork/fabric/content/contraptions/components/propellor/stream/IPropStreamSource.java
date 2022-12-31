d8945e66c9f054501151bdc5dd551aed67d97563
ThePlasticPotato <77798144+ThePlasticPotato@users.noreply.github.com>
2022-12-30 21:36:54 -0500

    fixed(?) prop spinup and partially added streams

---
+++ fabric/src/main/java/org/valkyrienskies/clockwork/fabric/content/contraptions/components/propellor/stream/IPropStreamSource.java
@@ -0,0 +1,44 @@
+package org.valkyrienskies.clockwork.fabric.content.contraptions.components.propellor.stream;
+
+import javax.annotation.Nullable;
+
+import com.simibubi.create.foundation.config.AllConfigs;
+import com.simibubi.create.foundation.config.CKinetics;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.Level;
+import org.joml.Vector3d;
+
+@MethodsReturnNonnullByDefault
+public interface IPropStreamSource {
+    @Nullable
+    PropStream getStream();
+
+    @Nullable
+    Level getStreamWorld();
+
+    BlockPos getStreamPos();
+
+    float getSpeed();
+
+    Direction getStreamOriginSide();
+
+    @Nullable
+    Direction getStreamDirection();
+
+    Vector3d getStreamScale();
+
+    default float getMaxDistance() {
+        float speed = Math.abs(this.getSpeed());
+        CKinetics config = AllConfigs.SERVER.kinetics;
+        float distanceFactor = Math.min(speed / config.fanRotationArgmax.get(), 1);
+        float pushDistance = Mth.lerp(distanceFactor, 3, config.fanPushDistance.get());
+        float pullDistance = Mth.lerp(distanceFactor, 3f, config.fanPullDistance.get());
+        return this.getSpeed() > 0 ? pushDistance : pullDistance;
+    }
+
+    boolean isSourceRemoved();
+}
--- fabric/src/main/java/org/valkyrienskies/clockwork/fabric/content/curiosities/particles/PropellorStreamParticle.java
+++ fabric/src/main/java/org/valkyrienskies/clockwork/fabric/content/curiosities/particles/PropellorStreamParticle.java
@@ -1,19 +1,25 @@
 package org.valkyrienskies.clockwork.fabric.content.curiosities.particles;
 
 import com.simibubi.create.Create;
+import com.simibubi.create.content.contraptions.components.fan.IAirCurrentSource;
 import com.simibubi.create.content.curiosities.bell.BasicParticleData;
 import com.simibubi.create.foundation.utility.VecHelper;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
+import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nonnull;
 
 public class PropellorStreamParticle extends SimpleAnimatedParticle {
-    Vec3 motion;
-    protected PropellorStreamParticle(ClientLevel level, double x, double y, double z, double dx, double dy, double dz, SpriteSet sprite) {
-        super(level, x, y ,z ,sprite, level.random.nextFloat()*.5f);
+
+    private final IAirCurrentSource source;
+    protected PropellorStreamParticle(ClientLevel world, IAirCurrentSource source, double x, double y, double z,
+                                      SpriteSet sprite) {
+        super(world, x, y ,z ,sprite, world.random.nextFloat()*.5f);
+        this.source = source;
         this.quadSize *= 0.75f;
         this.lifetime = 20;
         hasPhysics = false;
@@ -23,7 +29,6 @@ public class PropellorStreamParticle extends SimpleAnimatedParticle {
         this.xo = x;
         this.yo = y;
         this.zo = z;
-        motion=new Vec3(dx,dy,dz);
         setAlpha(.25f);
     }
     @Nonnull
@@ -33,6 +38,11 @@ public class PropellorStreamParticle extends SimpleAnimatedParticle {
 
     @Override
     public void tick() {
+        if (source == null || source.isSourceRemoved()) {
+            dissipate();
+            return;
+        }
+
         super.tick();
         this.xo = this.x;
         this.yo = this.y;
@@ -40,16 +50,40 @@ public class PropellorStreamParticle extends SimpleAnimatedParticle {
         if (this.age++ >= this.lifetime) {
             remove();
         } else {
+            if (source.getAirCurrent() == null || !source.getAirCurrent().bounds.inflate(.25f).contains(x, y, z)) {
+                dissipate();
+                return;
+            }
             grabSprite((int) Mth.clamp((this.age / (float)this.lifetime)*8 + level.random.nextInt(1), 0, 7));
-            xd = motion.x;
-            yd = motion.y;
-            zd = motion.z;
+            Vec3 directionVec = Vec3.atLowerCornerOf(source.getAirCurrent().direction.getNormal());
+            Vec3 motion = directionVec.scale(1 / 8f);
+            if (!source.getAirCurrent().pushing)
+                motion = motion.scale(-1);
+
+            double distance = new Vec3(x, y, z).subtract(VecHelper.getCenterOf(source.getAirCurrentPos()))
+                    .multiply(directionVec).length() - .5f;
+            if (distance > source.getAirCurrent().maxDistance + 1 || distance < -.25f) {
+                dissipate();
+                return;
+            }
+            motion = motion.scale(source.getAirCurrent().maxDistance - (distance - 1f)).scale(.5f);
+            grabSprite((int) Mth.clamp((distance / source.getAirCurrent().maxDistance) * 8 + level.random.nextInt(4),
+                    0, 7));
             double friction = 0.2*motion.lengthSqr();
             friction=Math.min(friction,0.5f);
             motion=motion.scale(1.0-friction);
+            xd = motion.x;
+            yd = motion.y;
+            zd = motion.z;
+
             this.move(this.xd,this.yd,this.zd);
         }
     }
+
+    private void dissipate() {
+        remove();
+    }
+
     private void grabSprite(int index) {
         setSprite(sprites.get(index, 8));
     }
@@ -61,8 +95,12 @@ public class PropellorStreamParticle extends SimpleAnimatedParticle {
             this.spriteSet = animSprite;
         }
 
-        public Particle createParticle(PropellorStreamParticleData data, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-            return new PropellorStreamParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+        public Particle createParticle(PropellorStreamParticleData data, ClientLevel worldIn, double x, double y, double z,
+                                       double xSpeed, double ySpeed, double zSpeed) {
+            BlockEntity te = worldIn.getBlockEntity(new BlockPos(data.posX, data.posY, data.posZ));
+            if (!(te instanceof IAirCurrentSource))
+                te = null;
+            return new PropellorStreamParticle(worldIn, (IAirCurrentSource) te, x, y, z, this.spriteSet);
         }
     }
 }
