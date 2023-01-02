a98bf45b6938bd51a53dc71d6972c99a18600fec
Fluffy Jenkins <richardjrn.w@gmail.com>
2023-01-02 01:09:18 +0000

    Improved Contraption interaction on ships and made train relocation work on ships

--- fabric/src/main/java/org/valkyrienskies/clockwork/fabric/mixin/create/client/MixinContraptionHandlerClient.java
+++ fabric/src/main/java/org/valkyrienskies/clockwork/fabric/mixin/create/client/MixinContraptionHandlerClient.java
@@ -2,11 +2,21 @@ package org.valkyrienskies.clockwork.fabric.mixin.create.client;
 
 import com.simibubi.create.content.contraptions.components.structureMovement.ContraptionHandlerClient;
 import com.simibubi.create.foundation.utility.Couple;
+
+import java.util.Iterator;
 import java.util.List;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Matrix4d;
+import org.joml.Matrix4f;
+import org.joml.Quaterniond;
 import org.joml.Vector3d;
+import org.joml.primitives.AABBd;
+import org.joml.primitives.AABBic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,31 +29,33 @@ import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 @Mixin(ContraptionHandlerClient.class)
 public abstract class MixinContraptionHandlerClient {
 
-    @Inject(
-        method = "getRayInputs",
-        at = @At(
-            value = "INVOKE",
-            target = "Lcom/simibubi/create/foundation/utility/Couple;create(Ljava/lang/Object;Ljava/lang/Object;)Lcom/simibubi/create/foundation/utility/Couple;"
-        ), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true
-    )
-    private static void redirectedOrigin(
-        final LocalPlayer player, final CallbackInfoReturnable<Couple<Vec3>> cir, final Minecraft mc,
-        Vec3 origin, final double reach, Vec3 target) {
-        final Ship ship = VSGameUtilsKt.getShipManagingPos(player.level, player.getOnPos());
+    @Inject(method = "getRayInputs", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/foundation/utility/Couple;create(Ljava/lang/Object;Ljava/lang/Object;)Lcom/simibubi/create/foundation/utility/Couple;"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void redirectedOrigin(final LocalPlayer player, final CallbackInfoReturnable<Couple<Vec3>> cir, final Minecraft mc, Vec3 origin, final double reach, Vec3 target) {
 
-        if (ship != null) {
-            final List<Vector3d>
-                originShips =
-                VSGameUtilsKt.transformToNearbyShipsAndWorld(player.level, origin.x, origin.y, origin.z, 10);
-            if (!originShips.isEmpty()) {
-                origin = VectorConversionsMCKt.toMinecraft(originShips.get(0));
-            }
+        if (mc.hitResult != null) {
+            //Vec3 searchLocation = mc.hitResult.getLocation();
+            AABB searchAABB = new AABB(origin, target).inflate(0.25, 2, 0.25);
+            final Iterator<Ship> ships = VSGameUtilsKt.getShipsIntersecting(player.level, searchAABB).iterator();
 
-            final List<Vector3d>
-                targetShips =
-                VSGameUtilsKt.transformToNearbyShipsAndWorld(player.level, target.x, target.y, target.z, 10);
-            if (!targetShips.isEmpty()) {
-                target = VectorConversionsMCKt.toMinecraft(targetShips.get(0));
+            if (ships.hasNext()) {
+                Ship ship = ships.next();
+
+                Matrix4d world2Ship = (Matrix4d) ship.getTransform().getWorldToShip();
+                AABBic shAABBi = ship.getShipAABB();
+                AABB shipAABB = new AABB(shAABBi.minX(), shAABBi.minY(), shAABBi.minZ(), shAABBi.maxX(), shAABBi.maxY(), shAABBi.maxZ());
+
+
+                origin = VectorConversionsMCKt.toMinecraft(world2Ship.transformPosition(VectorConversionsMCKt.toJOML(origin)));
+                target = VectorConversionsMCKt.toMinecraft(world2Ship.transformPosition(VectorConversionsMCKt.toJOML(target)));
+
+                Quaterniond tempQuat = new Quaterniond();
+                if (player.getVehicle() != null && player.getVehicle().getBoundingBox().intersects(shipAABB.inflate(20))) {
+                    ship.getTransform().getWorldToShip().getNormalizedRotation(tempQuat);
+                    tempQuat.invert();
+                    Vector3d offset = VectorConversionsMCKt.toJOML(target.subtract(origin));
+                    tempQuat.transform(offset);
+                    target = origin.add(VectorConversionsMCKt.toMinecraft(offset));
+                }
             }
         }
         cir.setReturnValue(Couple.create(origin, target));
