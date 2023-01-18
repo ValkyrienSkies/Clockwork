ee8cb34ab5168865506e4cc827a1f386f4387df6
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-01-18 20:05:32 +0100

    Fabric packets

--- fabric/src/main/java/org/valkyrienskies/clockwork/platform/fabric/SharedValuesImpl.java
+++ fabric/src/main/java/org/valkyrienskies/clockwork/platform/fabric/SharedValuesImpl.java
@@ -3,12 +3,9 @@ package org.valkyrienskies.clockwork.platform.fabric;
 import com.simibubi.create.foundation.item.render.CustomRenderedItemModelRenderer;
 import com.tterrag.registrate.util.entry.EntityEntry;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import org.valkyrienskies.clockwork.content.curiosities.tools.bluperglue.BluperGlueEntity;
 import org.valkyrienskies.clockwork.fabric.ClockWorkGroup;
-import org.valkyrienskies.clockwork.fabric.FabricClockWorkPackets;
 import org.valkyrienskies.clockwork.fabric.FabricClockworkEntities;
 import org.valkyrienskies.clockwork.platform.CWItem;
 import org.valkyrienskies.clockwork.platform.api.network.PacketChannel;
@@ -17,6 +14,7 @@ import java.util.function.BiConsumer;
 
 public class SharedValuesImpl {
     private static final CreativeModeTab TAB = new ClockWorkGroup();
+    private static final PacketChannel CHANNEL = new PacketChannelImpl();
 
     public static CreativeModeTab creativeTab() {
         return TAB;
@@ -27,8 +25,8 @@ public class SharedValuesImpl {
     }
 
     public static PacketChannel getPacketChannel() {
-        return FabricClockWorkPackets.INSTANCE;
-    } // TODO
+        return CHANNEL;
+    }
 
     public static BiConsumer<CWItem, CustomRenderedItemModelRenderer<?>> customRenderedRegisterer() {
         return BuiltinItemRendererRegistry.INSTANCE::register;
