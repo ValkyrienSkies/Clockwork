faca00b38324ba1b87ff4a559778e71410f9bb57
Rubydesic <rubydesic@gmail.com>
2023-01-27 15:48:18 -0500

    remove: strange conditional renderer

--- common/src/main/java/org/valkyrienskies/clockwork/client/render/scanner/ScannerRenderer.java
+++ common/src/main/java/org/valkyrienskies/clockwork/client/render/scanner/ScannerRenderer.java
@@ -4,15 +4,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.world.phys.Vec3;
 import org.valkyrienskies.clockwork.content.contraptions.infuser.PhysicsInfuserBlockEntity;
 import org.valkyrienskies.core.api.ships.ClientShip;
-import org.valkyrienskies.mod.compat.VSRenderer;
-import org.valkyrienskies.mod.mixin.ValkyrienCommonMixinConfigPlugin;
 
 import javax.annotation.Nullable;
 
 public interface ScannerRenderer {
-    ScannerRenderer INSTANCE =
-            ValkyrienCommonMixinConfigPlugin.getVSRenderer() == VSRenderer.VANILLA ?
-                    new ShipScannerRenderer() : new WorldScannerRenderer();
+    ScannerRenderer INSTANCE = new ShipScannerRenderer();
+//            ValkyrienCommonMixinConfigPlugin.getVSRenderer() == VSRenderer.VANILLA ?
+//                    new ShipScannerRenderer() : new WorldScannerRenderer();
 
     void doRender(final PoseStack poseStack);
 
