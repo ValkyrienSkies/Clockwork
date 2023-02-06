c27040fe148c50c7cf5b9f4024242752a69522bc
Fluffy Jenkins <richardjrn.w@gmail.com>
2023-02-06 01:57:22 +0000

    Some ship contraption actors can now interact with the world, Sticker now sticks together ship/ship ship/world world/ship, Fixed create centered actor bug

--- common/src/main/java/org/valkyrienskies/clockwork/mixin/compat/entity/MixinAbstractContraptionEntity.java
+++ common/src/main/java/org/valkyrienskies/clockwork/mixin/compat/entity/MixinAbstractContraptionEntity.java
@@ -1,15 +1,29 @@
 package org.valkyrienskies.clockwork.mixin.compat.entity;
 
+import com.simibubi.create.CreateClient;
+import com.simibubi.create.content.contraptions.components.actors.BlockBreakingMovementBehaviour;
+import com.simibubi.create.content.contraptions.components.actors.HarvesterMovementBehaviour;
+import com.simibubi.create.content.contraptions.components.actors.PloughMovementBehaviour;
 import com.simibubi.create.content.contraptions.components.actors.SeatEntity;
-import com.simibubi.create.content.contraptions.components.structureMovement.AbstractContraptionEntity;
-import com.simibubi.create.content.contraptions.components.structureMovement.Contraption;
+import com.simibubi.create.content.contraptions.components.deployer.DeployerFakePlayer;
+import com.simibubi.create.content.contraptions.components.deployer.DeployerMovementBehaviour;
+import com.simibubi.create.content.contraptions.components.structureMovement.*;
 import com.simibubi.create.foundation.utility.VecHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.CocoaBlock;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -20,15 +34,30 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.valkyrienskies.clockwork.mixin.compat.IMixinDeployerHandler;
+import org.valkyrienskies.clockwork.mixin.compat.IMixinDeployerMovementBehaviour;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 
+import java.util.Iterator;
+
+import static org.valkyrienskies.mod.common.util.VectorConversionsMCKt.toJOML;
+import static org.valkyrienskies.mod.common.util.VectorConversionsMCKt.toMinecraft;
+
 @Mixin(AbstractContraptionEntity.class)
 public abstract class MixinAbstractContraptionEntity extends Entity {
+
+    public MixinAbstractContraptionEntity(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Unique
     private static final Logger LOGGER = LogManager.getLogger("Clockwork.MixinAbstractContraptionEntity");
+
     @Shadow(remap = false)
     protected Contraption contraption;
 
@@ -44,14 +73,11 @@ public abstract class MixinAbstractContraptionEntity extends Entity {
     @Shadow
     public abstract Vec3 getPrevAnchorVec();
 
-    public MixinAbstractContraptionEntity(EntityType<?> entityType, Level level) {
-        super(entityType, level);
-    }
-
+    //Region start - fix being sent to the  ̶s̶h̶a̶d̶o̶w̶r̶e̶a̶l̶m̶ shipyard on ship contraption disassembly
     @Redirect(method = "moveCollidedEntitiesOnDisassembly", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;setPos(DDD)V"))
     private void redirectSetPos(Entity instance, double x, double y, double z) {
         Vector3d result = transformPosition(instance, x, y, z);
-        if (instance.position().distanceTo(VectorConversionsMCKt.toMinecraft(result)) < 20) {
+        if (instance.position().distanceTo(toMinecraft(result)) < 20) {
             instance.setPos(result.x, result.y, result.z);
         } else LOGGER.warn("Warning distance too high ignoring setPos request");
     }
@@ -59,7 +85,7 @@ public abstract class MixinAbstractContraptionEntity extends Entity {
     @Redirect(method = "moveCollidedEntitiesOnDisassembly", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;teleportTo(DDD)V"))
     private void redirectTeleportTo(Entity instance, double x, double y, double z) {
         Vector3d result = transformPosition(instance, x, y, z);
-        if (instance.position().distanceTo(VectorConversionsMCKt.toMinecraft(result)) < 20) {
+        if (instance.position().distanceTo(toMinecraft(result)) < 20) {
             instance.teleportTo(result.x, result.y, result.z);
         } else LOGGER.warn("Warning distance too high ignoring teleportTo request");
     }
@@ -74,6 +100,8 @@ public abstract class MixinAbstractContraptionEntity extends Entity {
         return newPos;
     }
 
+    //Region end
+    //Region start - fix entity rider position on ship contraptions
     @Override
     public void positionRider(@NotNull Entity passenger) {
         if (!hasPassenger(passenger))
@@ -83,19 +111,18 @@ public abstract class MixinAbstractContraptionEntity extends Entity {
             return;
         Vec3 riderPos = new Vec3(transformedVector.x, transformedVector.y + SeatEntity.getCustomEntitySeatOffset(passenger) - 1 / 8f, transformedVector.z);
 
-        Ship ship = VSGameUtilsKt.getShipManagingPos(passenger.level,riderPos.x,riderPos.y,riderPos.z);
-        if(VSGameUtilsKt.isBlockInShipyard(passenger.level,riderPos.x,riderPos.y,riderPos.z) && ship!=null){
+        Ship ship = VSGameUtilsKt.getShipManagingPos(passenger.level, riderPos.x, riderPos.y, riderPos.z);
+        if (VSGameUtilsKt.isBlockInShipyard(passenger.level, riderPos.x, riderPos.y, riderPos.z) && ship != null) {
             Vector3d tempVec = VectorConversionsMCKt.toJOML(riderPos);
-            ship.getShipToWorld().transformPosition(tempVec,tempVec);
-            riderPos = VectorConversionsMCKt.toMinecraft(tempVec);
+            ship.getShipToWorld().transformPosition(tempVec, tempVec);
+            riderPos = toMinecraft(tempVec);
         }
         passenger.setPos(riderPos);
     }
 
     @Inject(method = "toGlobalVector(Lnet/minecraft/world/phys/Vec3;FZ)Lnet/minecraft/world/phys/Vec3;",
             at = @At("HEAD"), cancellable = true)
-    private void redirectToGlobalVector(Vec3 localVec, final float partialTicks, final boolean prevAnchor,
-                                        final CallbackInfoReturnable<Vec3> cir) {
+    private void redirectToGlobalVector(Vec3 localVec, final float partialTicks, final boolean prevAnchor, final CallbackInfoReturnable<Vec3> cir) {
         if (partialTicks != 1 && !prevAnchor) {
             final Vec3 anchor = getAnchorVec();
             final Vec3 oldAnchor = getPrevAnchorVec();
@@ -113,4 +140,108 @@ public abstract class MixinAbstractContraptionEntity extends Entity {
             cir.setReturnValue(localVec);
         }
     }
+
+    //Region end
+    //Region start - Ship contraption actors affecting world
+    @Shadow
+    public abstract Vec3 toGlobalVector(Vec3 localVec, float partialTicks);
+
+    @Unique
+    private static Boolean isValidBlock(Level world, BlockPos colliderPos, MovementBehaviour movementBehaviour, MovementContext context) {
+        BlockState worldBlockState = world.getBlockState(colliderPos);
+        if (worldBlockState.getBlock() instanceof CocoaBlock)
+            return false;
+        if (movementBehaviour != null) {
+            if (movementBehaviour instanceof DeployerMovementBehaviour && context.temporaryData instanceof DeployerFakePlayer player) {
+                ((IMixinDeployerMovementBehaviour) movementBehaviour).invokeTryGrabbingItem(context);
+                if ((player.getMainHandItem().getItem() instanceof BlockItem blockItem) && (blockItem.getBlock() instanceof CropBlock)) {
+                    return IMixinDeployerHandler.invokeShouldActivate(player.getMainHandItem(), context.world, colliderPos, null);
+                } else
+                    return false;
+            }
+            if (movementBehaviour instanceof PloughMovementBehaviour behaviour) {
+                return worldBlockState.isAir() || behaviour.canBreak(world, colliderPos, worldBlockState);
+            }
+            if (movementBehaviour instanceof HarvesterMovementBehaviour harvesterMovementBehaviour) {
+                return harvesterMovementBehaviour.isValidCrop(world, colliderPos, worldBlockState)
+                        || harvesterMovementBehaviour.isValidOther(world, colliderPos, worldBlockState);
+            }
+            if (movementBehaviour instanceof BlockBreakingMovementBehaviour behaviour) {
+                return behaviour.canBreak(world, colliderPos, worldBlockState);
+            }
+        }
+        return !worldBlockState.isAir();
+    }
+
+    @Unique
+    private boolean shouldMod(MovementBehaviour moveBehaviour) {
+        return ((moveBehaviour instanceof BlockBreakingMovementBehaviour) || (moveBehaviour instanceof HarvesterMovementBehaviour) || (moveBehaviour instanceof DeployerMovementBehaviour));
+    }
+
+    @Unique
+    private StructureTemplate.StructureBlockInfo structureBlockInfo;
+
+    @Inject(method = "tickActors", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/AllMovementBehaviours;getBehaviour(Lnet/minecraft/world/level/block/state/BlockState;)Lcom/simibubi/create/content/contraptions/components/structureMovement/MovementBehaviour;"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void injectTickActors(CallbackInfo ci, boolean stalledPreviously, Iterator<MutablePair<StructureTemplate.StructureBlockInfo, MovementContext>> var2, MutablePair<StructureTemplate.StructureBlockInfo, MovementContext> pair, MovementContext context, StructureTemplate.StructureBlockInfo blockInfo) {
+        structureBlockInfo = blockInfo;
+    }
+
+    @Unique
+    private BlockPos getTargetPos(MovementBehaviour instance, MovementContext context, BlockPos pos, StructureTemplate.StructureBlockInfo blockInfo) {
+        if (shouldMod(instance) && context.world.getBlockState(pos).isAir() && VSGameUtilsKt.isBlockInShipyard(context.world, pos)) {
+            Ship ship = VSGameUtilsKt.getShipManagingPos(context.world, pos);
+            if (ship != null) {
+                Vector3d searchPos = toJOML(toGlobalVector(VecHelper.getCenterOf(blockInfo.pos)
+                        .add(instance.getActiveAreaOffset(context)), 1));
+                Vector3d searchPos2 = new Vector3d(searchPos);
+                int ttl = 100;
+                if (false && instance instanceof DeployerMovementBehaviour) {
+                    if (blockInfo.state.hasProperty(DirectionalBlock.FACING)) {
+                        for (int i = 1; i <= 10; i++) {
+                            BlockPos testPos = new BlockPos(searchPos.x, searchPos.y, searchPos.z).relative(blockInfo.state.getValue(DirectionalBlock.FACING), i);
+                            boolean check = !context.world.getBlockState(testPos).isAir();
+                            if (level.isClientSide && Minecraft.getInstance().options.renderDebug) {
+                                CreateClient.OUTLINER.showAABB("actorPosDeployerDebug-" + context.localPos + "-" + context.contraption.anchor, new AABB(testPos.getX(), testPos.getY(), testPos.getZ(), testPos.getX() + 1, testPos.getY() + 1, testPos.getZ() + 1), ttl)
+                                        .colored(check ? 0xff7000 : 0x70ff00)
+                                        .lineWidth(2 / 16f);
+                            }
+                            if (check)
+                                return pos;
+                        }
+                    }
+                }
+
+                Vec3 transformedSearchPos = toMinecraft(ship.getShipToWorld().transformPosition(searchPos, new Vector3d()));
+                if (blockInfo.state.hasProperty(DirectionalBlock.FACING)) {
+                    searchPos2.add(toJOML(
+                            Vec3.atLowerCornerOf(blockInfo.state.getValue(DirectionalBlock.FACING).getNormal())
+                    ));
+                } else searchPos2.add(0, 0, 1);
+                Vec3 transformedSearchPos2 = toMinecraft(ship.getShipToWorld().transformPosition(searchPos2, new Vector3d()));
+                BlockPos blockPos = new BlockPos(transformedSearchPos);
+                boolean check = isValidBlock(context.world, blockPos, instance, context);
+                if (level.isClientSide) {
+                    boolean debugView = Minecraft.getInstance().options.renderDebug;
+                    if (debugView) {
+
+                        CreateClient.OUTLINER.showLine("actorPosDebugLine-" + context.localPos + "-" + context.contraption.anchor, transformedSearchPos, transformedSearchPos2)
+                                .colored(0x0070ff)
+                                .lineWidth(2 / 32f);
+                        CreateClient.OUTLINER.showAABB("actorPosDebug-" + context.localPos + "-" + context.contraption.anchor, new AABB(blockPos.getX(), blockPos.getY(), blockPos.getZ(), blockPos.getX() + 1, blockPos.getY() + 1, blockPos.getZ() + 1), ttl)
+                                .colored(check ? 0xff7000 : 0x70ff00)
+                                .lineWidth(2 / 16f);
+                    }
+                }
+                if (check)
+                    pos = blockPos;
+            }
+        }
+        return pos;
+    }
+
+    @Redirect(method = "tickActors", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/contraptions/components/structureMovement/MovementBehaviour;visitNewPosition(Lcom/simibubi/create/content/contraptions/components/structureMovement/MovementContext;Lnet/minecraft/core/BlockPos;)V"))
+    private void redirectVisitNewPosition(MovementBehaviour instance, MovementContext context, BlockPos pos) {
+        instance.visitNewPosition(context, getTargetPos(instance, context, pos, structureBlockInfo));
+    }
+    //Region end
 }
---
+++ fabric/src/main/java/org/valkyrienskies/clockwork/fabric/mixin/compat/MixinStickerTileEntity.java
@@ -0,0 +1,188 @@
+package org.valkyrienskies.clockwork.fabric.mixin.compat;
+
+import com.simibubi.create.content.contraptions.components.structureMovement.chassis.StickerBlock;
+import com.simibubi.create.content.contraptions.components.structureMovement.chassis.StickerTileEntity;
+import com.simibubi.create.foundation.tileEntity.SmartTileEntity;
+import com.simibubi.create.foundation.utility.animation.LerpedFloat;
+import com.tterrag.registrate.fabric.EnvExecutor;
+import net.fabricmc.api.EnvType;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.SupportType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Quaterniond;
+import org.joml.Quaterniondc;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.clockwork.content.curiosities.tools.bluperglue.BluperGlueItem;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.core.apigame.constraints.VSAttachmentOrientationConstraint;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+import java.util.Iterator;
+
+import static org.valkyrienskies.mod.common.util.VectorConversionsMCKt.toJOML;
+import static org.valkyrienskies.mod.common.util.VectorConversionsMCKt.toMinecraft;
+
+@Mixin(StickerTileEntity.class)
+public abstract class MixinStickerTileEntity extends SmartTileEntity {
+    public MixinStickerTileEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Shadow
+    public abstract boolean isAttachedToBlock();
+
+    @Shadow
+    private LerpedFloat piston;
+
+
+    @Shadow
+    public abstract void playSound(boolean attach);
+
+    @Shadow
+    public abstract boolean isBlockStateExtended();
+
+    @Unique
+    private boolean lastExtended = false;
+
+    @Unique
+    private void removeConstraint(ServerLevel level) {
+        if (getExtraCustomData().contains("ShipStickerConstraint")) {
+            VSGameUtilsKt.getShipObjectWorld(level).removeConstraint(getExtraCustomData().getInt("ShipStickerConstraint"));
+            getExtraCustomData().remove("ShipStickerConstraint");
+        }
+    }
+
+    @Unique
+    private void doAttach(ServerLevel level, Ship ship1, Ship ship2, Vector3d myPos, Direction myDir) {
+        if (ship1 == null && ship2 == null)
+            return;
+        boolean world2Ship = false;
+        if (ship1 == null) {
+            ship1 = ship2;
+            ship2 = null;
+            world2Ship = true;
+        }
+
+        removeConstraint(level);
+
+        Vector3d myDirNormal = toJOML(Vec3.atLowerCornerOf(myDir.getNormal()));
+        myDirNormal.mul(0.5);
+        Vector3d ship1ConstraintPos = myPos.add(myDirNormal);
+        Vector3d ship2ConstraintPos = new Vector3d(ship1ConstraintPos);
+
+        if(!world2Ship)
+            ship1.getShipToWorld().transformPosition(ship2ConstraintPos, ship2ConstraintPos);
+        else
+            ship1.getWorldToShip().transformPosition(ship1ConstraintPos, ship1ConstraintPos);
+
+        long ship2Id;
+        Quaterniondc ship2Rotation = new Quaterniond();
+
+        if (ship2 == null) {
+            ship2Id = VSGameUtilsKt.getShipObjectWorld(level).getDimensionToGroundBodyIdImmutable().get(VSGameUtilsKt.getDimensionId(level));
+        } else {
+            ship2Id = ship2.getId();
+            ship2.getWorldToShip().transformPosition(ship2ConstraintPos, ship2ConstraintPos);
+            ship2Rotation = ship2.getTransform().getShipToWorldRotation();
+        }
+
+        VSAttachmentOrientationConstraint constraint = new VSAttachmentOrientationConstraint(
+                ship1.getId(),
+                ship2Id,
+                1e-9 / VSGameUtilsKt.getShipObjectWorld(level).getAllShips().getById(ship1.getId()).getInertiaData().getMass(),
+                ship1ConstraintPos,
+                ship2ConstraintPos,
+                1e20,
+                ship1.getTransform().getShipToWorldRotation(),
+                ship2Rotation,
+                1e20
+        );
+        Integer constraintID = VSGameUtilsKt.getShipObjectWorld(level).createNewConstraint(constraint);
+        getExtraCustomData().putInt("ShipStickerConstraint", constraintID.intValue());
+    }
+
+    @Unique
+    private boolean isAttachedToShipOrWorld(boolean attach) {
+        boolean result = false;
+        Vector3d myPos = toJOML(Vec3.atLowerCornerOf(getBlockPos()));
+        if (level == null)
+            return false;
+        Ship ship = VSGameUtilsKt.getShipManagingPos(level, myPos);
+        Ship ship2 = null;
+        Vector3d myPosCentered = toJOML(Vec3.atCenterOf(getBlockPos()));
+        Direction myDir = this.getBlockState().getValue(DirectionalBlock.FACING);
+        Vector3d myDirNormal = toJOML(Vec3.atLowerCornerOf(myDir.getNormal()));
+
+        Vector3d searchPos = myPosCentered.add(myDirNormal);
+        if (ship != null)
+            ship.getShipToWorld().transformPosition(searchPos, searchPos);
+
+        BlockState worldBlockState = level.getBlockState(new BlockPos(toMinecraft(searchPos)));
+        if (!worldBlockState.isAir()) {
+            result = true;
+        } else {
+            double bounds = 0.5;
+            AABB searchAABB = new AABB(searchPos.x - bounds, searchPos.y - bounds, searchPos.z - bounds,
+                    searchPos.x + bounds, searchPos.y + bounds, searchPos.z + bounds);
+
+            Iterator<Ship> ships = VSGameUtilsKt.getShipsIntersecting(level, searchAABB).iterator();
+            Ship shipItr;
+            Vector3d transformedSearchPos = searchPos;
+            if (ships.hasNext()) {
+                do {
+                    shipItr = ships.next();
+                    if (shipItr == ship) continue;
+                    shipItr.getWorldToShip().transformPosition(searchPos, transformedSearchPos);
+                    BlockPos blockPos = new BlockPos(toMinecraft(transformedSearchPos));
+                    BlockState blockState = level.getBlockState(blockPos);
+                    if (!blockState.isAir() && blockState.isFaceSturdy(level, blockPos, Direction.UP, SupportType.RIGID)) {
+                        result = true;
+                        ship2 = shipItr;
+                    }
+                } while (ships.hasNext() && !result);
+            }
+        }
+        if (result && !level.isClientSide && attach)
+            doAttach((ServerLevel) level, ship, ship2, myPos, myDir);
+
+        return result;
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"), remap = false)
+    private void injectTick(CallbackInfo ci) {
+        if (level == null)
+            return;
+        boolean blockAttached = isAttachedToBlock();
+        boolean shipAttached = isAttachedToShipOrWorld(false);
+        if (!blockAttached && piston.getValue(0) != piston.getValue() && piston.getValue() == 1 && shipAttached) {
+            if (level.isClientSide) {
+                BluperGlueItem.spawnParticles(level, worldPosition, getBlockState().getValue(StickerBlock.FACING), true);
+                EnvExecutor.runWhenOn(EnvType.CLIENT, () -> () -> playSound(true));
+            }
+        }
+        if (!level.isClientSide) {
+            if (lastExtended != isBlockStateExtended()) {
+                if (isBlockStateExtended()) {
+                    if (!blockAttached && shipAttached) {
+                        isAttachedToShipOrWorld(true);
+                    }
+                } else {
+                    removeConstraint((ServerLevel) level);
+                }
+            }
+        }
+        lastExtended = isBlockStateExtended();
+    }
+}
