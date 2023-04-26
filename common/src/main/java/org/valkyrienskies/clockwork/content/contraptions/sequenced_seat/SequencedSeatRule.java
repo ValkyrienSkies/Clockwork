0275a040a817ac910de40813b7d21c3334e97f01
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-04-26 12:07:17 +0200

    Fixing sequenced seat angle, still very wip.

--- common/src/main/java/org/valkyrienskies/clockwork/content/contraptions/sequenced_seat/SequencedSeatRule.java
+++ common/src/main/java/org/valkyrienskies/clockwork/content/contraptions/sequenced_seat/SequencedSeatRule.java
@@ -1,12 +1,13 @@
 package org.valkyrienskies.clockwork.content.contraptions.sequenced_seat;
 
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public record SequencedSeatRule(Set<InputKey> inputKeys, SequencedSeatOperation operation, SequencedSeatValue value) {
+public class SequencedSeatRule {
 
     public static SequencedSeatRule empty() {
         return new SequencedSeatRule(new HashSet<>(), SequencedSeatOperation.NOTHING, null);
@@ -24,37 +25,80 @@ public record SequencedSeatRule(Set<InputKey> inputKeys, SequencedSeatOperation 
         return new SequencedSeatRule(keys, operation, value);
     }
 
+    private final Set<InputKey> inputKeys;
+    private final SequencedSeatOperation operation;
+    private final SequencedSeatValue value;
+
+    public SequencedSeatRule(Set<InputKey> inputKeys, SequencedSeatOperation operation, SequencedSeatValue value) {
+        this.inputKeys = inputKeys;
+        this.operation = operation;
+        this.value = value;
+    }
+
     public boolean matches(Set<InputKey> inputKeys) {
         return inputKeys.equals(this.inputKeys);
     }
 
-    public float calculateModifier(SequencedSeatBlockEntity be) {
-        return keepTurning(be) ? switch (operation) {
+    public float calculateModifier(SequencedSeatBlockEntity be, Direction face, Set<InputKey> inputKeys) {
+        boolean matches = matches(inputKeys);
+        return switch (operation) {
             case NOTHING -> 0;
-            case TURN_ANGLE, TURN_DISTANCE -> 1;
-            case MULTIPLY -> ((SequencedSeatValue.MultiplyValue) value).multiplier;
-        } : 0;
+            case TURN_ANGLE -> angleRotate(be, face, matches);
+            case TURN_DISTANCE -> distanceRotate(be, face, matches);
+            case MULTIPLY -> matches ? ((SequencedSeatValue.MultiplyValue) value).multiplier : 0;
+        };
     }
 
-    private boolean keepTurning(SequencedSeatBlockEntity be) {
-        int duration = switch (operation) {
-            case TURN_ANGLE -> {
-                int target = ((SequencedSeatValue.AngleValue) value).degrees;
-                double degreesPerTick = KineticTileEntity.convertToAngular(be.getSpeed());
-                int ticks = (int) (target / degreesPerTick);
-                double degreesErr = target - degreesPerTick * (double) ticks;
-                yield ticks + (degreesPerTick > 2.0 * degreesErr ? 0 : 1);
-            }
-            case TURN_DISTANCE -> {
-                int target = ((SequencedSeatValue.DistanceValue) value).meters;
-                double metersPerTick = KineticTileEntity.convertToLinear(be.getSpeed());
-                yield (int) (target / metersPerTick + 1.0);
-            }
-            default -> Integer.MAX_VALUE;
-        };
+    private boolean inAction = false;
+    private float angleRotate(SequencedSeatBlockEntity be, Direction face, boolean matches) {
+        if (!inAction && !matches) return 0;
 
+        int targetDegrees = -((SequencedSeatValue.AngleValue) value).degrees;
 
-        return be.getTicksSinceLastUpdate() < duration;
+        float degreesAway = be.getDegreesAwayFromBase(face);
+        float diff = matches ? targetDegrees - degreesAway : -degreesAway;
+
+        if (diff > 180) diff -= 360;
+        else if (diff < -180) diff += 360;
+
+        if (diff < 0.1 && diff > -0.1) {
+            inAction = matches;
+            return 0;
+        }
+
+        inAction = true;
+        double degreesPerTick = KineticTileEntity.convertToAngular(be.getSpeed());
+        if (Math.abs(degreesPerTick) > Math.abs(diff)) {
+            return (float) (diff / degreesPerTick);
+        } else return (diff * degreesPerTick) < 0 ? -1 : 1;
+        // If diff and degrees per tick have different signs, we need to reverse the direction
+        // So we use * < to check that
+    }
+
+    private float distanceRotate(SequencedSeatBlockEntity be, Direction face, boolean matches) {
+        /*
+        if (!inAction && !matches) return 0;
+
+        int targetDegrees = ((SequencedSeatValue.DistanceValue) value).meters * 512;
+
+        float degreesAway = be.getDegreesAwayFromBase(face);
+        float diff = matches ? targetDegrees - degreesAway : -degreesAway;
+
+        if (diff > 180) diff -= 360;
+        else if (diff < -180) diff += 360;
+
+        if (diff < 0.1 && diff > -0.1) {
+            inAction = matches;
+            return 0;
+        }
+
+        inAction = true;
+        double metersPerTick = KineticTileEntity.convertToLinear(be.getSpeed());
+        if (Math.abs(metersPerTick) > Math.abs(diff)) {
+            return (float) (diff / metersPerTick);
+        } else return (diff * metersPerTick) < 0 ? -1 : 1;
+        */
+        return 1; //TODO
     }
 
     public CompoundTag serializeNBT() {
@@ -67,5 +111,17 @@ public record SequencedSeatRule(Set<InputKey> inputKeys, SequencedSeatOperation 
 
 
         return tag;
+    }
+
+    public Set<InputKey> getInputKeys() {
+        return inputKeys;
+    }
+
+    public SequencedSeatOperation getOperation() {
+        return operation;
+    }
+
+    public SequencedSeatValue getValue() {
+        return value;
     }
 }
