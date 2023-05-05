52c707657462db09a9ae175cd1073fb605f50283
ThePlasticPotato <77798144+ThePlasticPotato@users.noreply.github.com>
2023-05-05 17:09:03 -0400

    trioden fix

--- common/src/main/java/org/valkyrienskies/clockwork/mixin/compat/MixinContraption.java
+++ common/src/main/java/org/valkyrienskies/clockwork/mixin/compat/MixinContraption.java
@@ -9,17 +9,18 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Contraption.class)
 public class MixinContraption {
-    @WrapOperation(method = "onEntityCreated", at = {@At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z")})
-    private boolean wrapOp(Level world, Entity entity, Operation<Boolean> addFreshEntity, AbstractContraptionEntity abstractContraptionEntity) {
+    @Redirect(method = "onEntityCreated", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"))
+    private boolean wrapOp(Level level, Entity entity) {
         // BlockPos anchor = blockFace.getConnectedPos();
         // movedContraption.setPos(anchor.getX() + .5f, anchor.getY(), anchor.getZ() + .5f);
         //
         // Derive anchor from the code above
         final BlockPos anchor = new BlockPos((int) Math.floor(entity.getX()), (int) Math.floor(entity.getY()), (int) Math.floor(entity.getZ()));
-        boolean added = addFreshEntity.call(world, entity);
+        boolean added = level.addFreshEntity(entity);
         if (added) {
             entity.moveTo(anchor.getX() + .5, anchor.getY(), anchor.getZ() + .5);
         }
