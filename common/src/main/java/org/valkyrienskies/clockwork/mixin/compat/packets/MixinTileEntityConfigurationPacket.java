95cd9db95434509fa4780940570c58e9d08aff7c
StewStrong <stew.strong.420@gmail.com>
2023-10-09 08:44:54 -0600

    Fixed MixinTileEntityConfigurationPacket on forge

--- common/src/main/java/org/valkyrienskies/clockwork/mixin/compat/packets/MixinTileEntityConfigurationPacket.java
+++ common/src/main/java/org/valkyrienskies/clockwork/mixin/compat/packets/MixinTileEntityConfigurationPacket.java
@@ -1,28 +1,21 @@
 package org.valkyrienskies.clockwork.mixin.compat.packets;
 
 import com.simibubi.create.foundation.networking.BlockEntityConfigurationPacket;
-import com.simibubi.create.foundation.networking.SimplePacketBase;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
-
-import java.util.function.Supplier;
 
 @Mixin(BlockEntityConfigurationPacket.class)
 public abstract class MixinTileEntityConfigurationPacket {
     @Unique
-    private Level world;
+    private Level _clockworkLevel;
 
     @Redirect(
             method = "lambda$handle$0",
@@ -33,22 +26,23 @@ public abstract class MixinTileEntityConfigurationPacket {
     )
     private boolean redirectCloserThan(final BlockPos instance, final Vec3i vec3i, final double v) {
         BlockPos blockPos = instance;
-        if (VSGameUtilsKt.isBlockInShipyard(this.world, instance)) {
-            final Ship ship = VSGameUtilsKt.getShipManagingPos(this.world, instance);
+        if (VSGameUtilsKt.isBlockInShipyard(this._clockworkLevel, instance)) {
+            final Ship ship = VSGameUtilsKt.getShipManagingPos(this._clockworkLevel, instance);
             final Vector3d tempVec = VSGameUtilsKt.toWorldCoordinates(ship, instance);
             blockPos = new BlockPos(tempVec.x, tempVec.y, tempVec.z);
         }
         return blockPos.closerThan(vec3i, v);
     }
 
-    @Inject(
+    @Redirect(
             method = "lambda$handle$0",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/level/Level;isLoaded(Lnet/minecraft/core/BlockPos;)Z"
-            ), locals = LocalCapture.CAPTURE_FAILHARD
+            )
     )
-    private void injectCaptureLevel(SimplePacketBase.Context context, CallbackInfo ci, ServerPlayer player, Level world) {
-        this.world = world;
+    private boolean injectCaptureLevel(final Level instance, final BlockPos pos) {
+        this._clockworkLevel = instance;
+        return instance.isLoaded(pos);
     }
 }
