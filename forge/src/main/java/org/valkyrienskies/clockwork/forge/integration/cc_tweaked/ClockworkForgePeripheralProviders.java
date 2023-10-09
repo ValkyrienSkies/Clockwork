4204a74af1a8f64e0bbc598be3f6ef242b50b1f7
StewStrong <stew.strong.420@gmail.com>
2023-10-09 08:22:47 -0600

    Got forge to compile (still not working tho)

--- forge/src/main/java/org/valkyrienskies/clockwork/forge/integration/cc_tweaked/ClockworkForgePeripheralProviders.java
+++ forge/src/main/java/org/valkyrienskies/clockwork/forge/integration/cc_tweaked/ClockworkForgePeripheralProviders.java
@@ -1,26 +1,13 @@
 package org.valkyrienskies.clockwork.forge.integration.cc_tweaked;
 
-import com.sun.jna.platform.win32.DdemlUtil;
 import dan200.computercraft.api.ComputerCraftAPI;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.peripheral.IPeripheralProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
-import org.valkyrienskies.clockwork.content.propulsion.afterblazer.AfterblazerEngineBlockEntity;
-import org.valkyrienskies.clockwork.content.propulsion.ballooner.BalloonerBlockEntity;
-import org.valkyrienskies.clockwork.content.kinetics.combustion_engine.CombustionEngineBlockEntity;
-import org.valkyrienskies.clockwork.content.contraptions.flap.FlapBearingBlockEntity;
-import org.valkyrienskies.clockwork.content.contraptions.phys.bearing.PhysBearingBlockEntity;
-import org.valkyrienskies.clockwork.content.contraptions.propellor.PropellorBearingBlockEntity;
-import org.valkyrienskies.clockwork.content.kinetics.sequenced_seat.SequencedSeatBlockEntity;
-import org.valkyrienskies.clockwork.content.materials.solids.colorblock.ColorBlockEntity;
-import org.valkyrienskies.clockwork.integration.cc.*;
-import org.valkyrienskies.clockwork.util.blocktype.ConnectedWingAlike;
 
 public class ClockworkForgePeripheralProviders {
     public static void register() {
@@ -31,6 +18,8 @@ public class ClockworkForgePeripheralProviders {
         @NotNull
         @Override
         public LazyOptional<IPeripheral> getPeripheral(@NotNull Level level, @NotNull BlockPos blockPos, @NotNull Direction direction) {
+            // TODO: Fix this
+            /*
             BlockEntity be = level.getBlockEntity(blockPos);
             if (be instanceof AfterblazerEngineBlockEntity afterblazer)
                 return LazyOptional.of(() -> new ForgeAfterblazerPeripheral(afterblazer));
@@ -48,6 +37,7 @@ public class ClockworkForgePeripheralProviders {
                 return LazyOptional.of(() -> new PhysBearingPeripheral(phys));
             else if (be instanceof ColorBlockEntity color)
                 return LazyOptional.of(() -> new ColorPeripheral(color));
+             */
             return LazyOptional.empty();
         }
     }
