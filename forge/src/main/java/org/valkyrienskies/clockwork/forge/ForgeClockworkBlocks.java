0a32c21df8935582386a748d7c1edc003829b440
mrsterner <simon@sterner.dev>
2023-12-13 01:20:51 +0100

    yeet forge branch, back to dev

--- forge/src/main/java/org/valkyrienskies/clockwork/forge/ForgeClockworkBlocks.java
+++ forge/src/main/java/org/valkyrienskies/clockwork/forge/ForgeClockworkBlocks.java
@@ -5,17 +5,7 @@ public class ForgeClockworkBlocks {
     static {
     }
 
-    //////// Propellor Bearing ////////
 
-//    public static final BlockEntry<ForgeCombustionEngineBlock> COMBUSTION_ENGINE =
-//            REGISTRATE.block("combustion_engine", ForgeCombustionEngineBlock::new)
-//                    .initialProperties(SharedProperties::copperMetal)
-//                    .properties(p -> p.color(MaterialColor.COLOR_ORANGE))
-//                    .transform(pickaxeOnly())
-//                    .blockstate((c, p) -> p.horizontalFaceBlock(c.get(), AssetLookup.partialBaseModel(c, p)))
-//                    .item()
-//                    .transform(customItemModel())
-//                    .register();
 
     public static void register() {
     }
--- forge/src/main/java/org/valkyrienskies/clockwork/forge/ForgeClockworkItems.java
+++ forge/src/main/java/org/valkyrienskies/clockwork/forge/ForgeClockworkItems.java
@@ -2,10 +2,6 @@ package org.valkyrienskies.clockwork.forge;
 
 public class ForgeClockworkItems {
 
-//    public static final ItemEntry<PastrymakerItem> PASTRYMAKER =
-//            REGISTRATE.item("pastrymaker", PastrymakerItem::new)
-//                    .model(AssetLookup.itemModelWithPartials())
-//                    .register();
 
     public static void register() {
     }
