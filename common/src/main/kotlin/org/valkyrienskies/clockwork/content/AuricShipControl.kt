2f40e94adec4107ac9a355d1d3a68d63b6f3a5b0
mrsterner <simon@sterner.dev>
2023-12-28 16:39:58 +0100

    auric ore tweaks

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/AuricShipControl.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/AuricShipControl.kt
@@ -1,15 +1,19 @@
 package org.valkyrienskies.clockwork.content
 
 import org.joml.Vector3d
+import org.valkyrienskies.clockwork.content.contraptions.phys.gyro.GyroShipControl
 import org.valkyrienskies.core.api.ships.*
 import org.valkyrienskies.core.impl.game.ships.PhysShipImpl
 
 class AuricShipControl : ShipForcesInducer {
 
+    var aurics = 0
+
     override fun applyForces(physShip: PhysShip) {
         physShip as PhysShipImpl
 
-        val force =  Vector3d(0.0,2.1,0.0).mul(5000.0, Vector3d())
+        val yForce = (aurics * 2.0) - 1.85
+        val force =  Vector3d(0.0, yForce,0.0).mul(5000.0, Vector3d())
 
         physShip.applyInvariantForce(force)
     }
