be74e561584411312e194e475ece388e13c86abd
mrsterner <simon@sterner.dev>
2023-12-28 16:39:58 +0100

    auric ore tweaks

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/AuricOreBlock.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/AuricOreBlock.kt
@@ -3,23 +3,25 @@ package org.valkyrienskies.clockwork.content
 import com.simibubi.create.content.materials.ExperienceBlock
 import com.simibubi.create.foundation.block.IBE
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.Rotation
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.BlockHitResult
 import org.valkyrienskies.clockwork.ClockworkBlockEntities
-import org.valkyrienskies.mod.common.dimensionId
-import org.valkyrienskies.mod.common.isBlockInShipyard
-import org.valkyrienskies.mod.common.shipObjectWorld
+import org.valkyrienskies.core.api.ships.LoadedServerShip
+import org.valkyrienskies.core.util.datastructures.DenseBlockPosSet
+import org.valkyrienskies.mod.common.*
+import org.valkyrienskies.mod.common.assembly.createNewShipWithBlocks
 import org.valkyrienskies.mod.common.util.toBlockPos
 import org.valkyrienskies.mod.common.util.toJOML
-import org.valkyrienskies.mod.common.yRange
 import org.valkyrienskies.mod.util.relocateBlock
 
 class AuricOreBlock(properties: Properties) : ExperienceBlock(properties) {
@@ -59,10 +61,37 @@ class AuricOreBlock(properties: Properties) : ExperienceBlock(properties) {
     }
 
     fun shipifyBlock(level: ServerLevel, blockPos: BlockPos)  {
-        val dimensionId = level.dimensionId
-        val serverShip = level.shipObjectWorld.createNewShipAtBlock(blockPos.toJOML(), false, 1.0, dimensionId)
-        val centerPos = serverShip.chunkClaim.getCenterBlockCoordinates(level.yRange).toBlockPos()
-        level.relocateBlock(blockPos, centerPos, true, serverShip, Rotation.NONE)
-        AuricShipControl.getOrCreate(serverShip)
+        val dense = DenseBlockPosSet()
+        dense.add(blockPos.x, blockPos.y, blockPos.z)
+
+        for (dir in Direction.values()) {
+            var dirPos = blockPos.relative(dir)
+            if (level.getBlockState(dirPos).`is`(this)) {
+                dense.add(dirPos.x, dirPos.y, dirPos.z)
+            }
+        }
+
+        val connectedShip = createNewShipWithBlocks(blockPos, dense, level)
+        AuricShipControl.getOrCreate(connectedShip).aurics += dense.size
+    }
+
+    override fun onPlace(state: BlockState, level: Level, pos: BlockPos, oldState: BlockState, movedByPiston: Boolean) {
+        if (level is ServerLevel) {
+            val ship = level.getShipManagingPos(pos)
+            if (ship != null) {
+                AuricShipControl.getOrCreate(ship).aurics ++
+            }
+        }
+        super.onPlace(state, level, pos, oldState, movedByPiston)
+    }
+
+    override fun destroy(level: LevelAccessor, pos: BlockPos, state: BlockState) {
+        if (level is ServerLevel) {
+            val ship = level.getShipManagingPos(pos)
+            if (ship != null) {
+                AuricShipControl.getOrCreate(ship).aurics --
+            }
+        }
+        super.destroy(level, pos, state)
     }
 }
