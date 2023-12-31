49250dab3d11a877d7c874e73d1c0dff2ed099b7
mrsterner <simon@sterner.dev>
2023-12-31 15:44:19 +0100

    add handel to mixin names

--- common/src/main/java/org/valkyrienskies/clockwork/mixinduck/MixinPlayerDuck.java
+++ common/src/main/java/org/valkyrienskies/clockwork/mixinduck/MixinPlayerDuck.java
@@ -3,7 +3,7 @@ package org.valkyrienskies.clockwork.mixinduck;
 import org.valkyrienskies.clockwork.content.curiosities.tools.gravitron.GravitronItem;
 
 public interface MixinPlayerDuck {
-    void vs_clockwork$setGravitronState(GravitronItem.Companion.GravitronState state);
+    void setGravitronState(GravitronItem.Companion.GravitronState state);
 
-    GravitronItem.Companion.GravitronState vs_clockwork$getGravitronState();
+    GravitronItem.Companion.GravitronState getGravitronState();
 }
