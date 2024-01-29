b59d49dea1b8c74863edd3c699dbebf1da50e1de
mrsterner <simon@sterner.dev>
2024-01-29 13:47:14 +0100

    dont push this yet you silly

---
+++ common/src/main/java/org/valkyrienskies/clockwork/mixin/MixinEntity.java
@@ -0,0 +1,45 @@
+package org.valkyrienskies.clockwork.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+
+    @Shadow @Final private Set<TagKey<Fluid>> fluidOnEyes;
+
+    @WrapOperation(method = "updateFluidOnEyes", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/Level;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;"))
+    private FluidState clockwork$submarine(Level level, BlockPos pos, Operation<FluidState> operation){
+
+        Entity entity = (Entity) (Object) this;
+        if (entity instanceof Player player) {
+            if (player.isShiftKeyDown()) {
+                this.fluidOnEyes.clear();
+                return Fluids.EMPTY.defaultFluidState();//operation.call(level, pos);
+            }
+        }
+        return operation.call(level, pos);
+    }
+
+    @WrapOperation(method = "updateFluidOnEyes", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/FluidState;getTags()Ljava/util/stream/Stream;"))
+    private Stream<TagKey<Fluid>> d(FluidState instance, Operation<Stream<TagKey<Fluid>>> original){
+        //System.out.println(instance);
+        return original.call(instance);
+    }
+}
