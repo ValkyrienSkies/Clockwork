f578ba2b6b9a7b617c19c9410503498e9a391157
mrsterner <simon@sterner.dev>
2024-02-01 19:14:55 +0100

    update from 1.18

--- common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkWorldgen.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkWorldgen.kt
@@ -28,7 +28,7 @@ object ClockworkWorldgen {
                             OreConfiguration.target(DEEPSLATE_ORE_REPLACEABLES, ClockworkBlocks.WANDERLITE_DEEPSLATE_ORE.get().defaultBlockState()),
                             OreConfiguration.target(BlockMatchTest(Blocks.END_STONE), ClockworkBlocks.WANDERLITE_END_ORE.get().defaultBlockState())
                         ),
-                        3))
+                        4))
 
             val placedFeature = PlacementUtils.register(ClockworkMod.MOD_ID + ":ore_wanderlite", configuredFeature,
                 listOf(
