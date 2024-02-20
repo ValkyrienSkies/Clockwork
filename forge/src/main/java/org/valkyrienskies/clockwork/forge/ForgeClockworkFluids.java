605637e06ff55f71c6f51ce6ecdf5edd2118ad97
mrsterner <simon@sterner.dev>
2024-02-20 22:22:16 +0100

    sync version

--- forge/src/main/java/org/valkyrienskies/clockwork/forge/ForgeClockworkFluids.java
+++ forge/src/main/java/org/valkyrienskies/clockwork/forge/ForgeClockworkFluids.java
@@ -8,7 +8,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraftforge.fluids.FluidAttributes;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 import org.valkyrienskies.clockwork.ClockworkMod;
 import org.valkyrienskies.clockwork.ClockworkSounds;
@@ -20,15 +19,5 @@ public class ForgeClockworkFluids {
 
     public static void register() {}
 
-    private static class NoColorFluidAttributes extends FluidAttributes {
 
-        protected NoColorFluidAttributes(Builder builder, Fluid fluid) {
-            super(builder, fluid);
-        }
-
-        @Override
-        public int getColor(BlockAndTintGetter level, BlockPos pos) {
-            return 0x00ffffff;
-        }
-    }
 }
