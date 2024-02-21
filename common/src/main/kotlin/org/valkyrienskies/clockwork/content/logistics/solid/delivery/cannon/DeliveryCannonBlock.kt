7bd15c474921a9b215312d6bd5b4a79c27200f60
mrsterner <simon@sterner.dev>
2024-02-21 13:12:10 +0100

    removed useless delivery blocks

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/solid/delivery/cannon/DeliveryCannonBlock.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/solid/delivery/cannon/DeliveryCannonBlock.kt
@@ -9,20 +9,4 @@ import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 import org.valkyrienskies.clockwork.ClockworkBlockEntities
 
-class DeliveryCannonBlock(properties: Properties) : KineticBlock(properties), IBE<DeliveryCannonBlockEntity> {
-    override fun getRotationAxis(state: BlockState): Direction.Axis {
-        return Direction.Axis.Y
-    }
-
-    override fun hasShaftTowards(world: LevelReader, pos: BlockPos, state: BlockState, face: Direction): Boolean {
-        return face == Direction.DOWN
-    }
-
-    override fun getBlockEntityClass(): Class<DeliveryCannonBlockEntity> {
-        return DeliveryCannonBlockEntity::class.java
-    }
-
-    override fun getBlockEntityType(): BlockEntityType<out DeliveryCannonBlockEntity> {
-        return ClockworkBlockEntities.DELIVERY_CANNON.get()
-    }
-}
+class DeliveryCannonBlock
