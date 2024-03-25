fcc8d17240ee09916bcf334c9bac0f33efbfd02a
mrsterner <simon@sterner.dev>
2024-03-25 23:09:49 +0100

    gyro ponder

--- common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkBlocks.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkBlocks.kt
@@ -18,6 +18,7 @@ import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.SoundType
+import net.minecraft.world.level.block.StairBlock
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.BlockState
 import org.valkyrienskies.clockwork.ClockworkMod.REGISTRATE
