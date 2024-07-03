1b64104dc4b63e6927f7b15b19601e65967c2b4c
Potato <77798144+ThePlasticPotato@users.noreply.github.com>
2024-07-03 10:44:45 -0400

    more missed references

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/gas/duct/DuctRenderer.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/gas/duct/DuctRenderer.kt
@@ -21,9 +21,13 @@ class DuctRenderer(context: BlockEntityRendererProvider.Context) : SmartBlockEnt
         super.renderSafe(blockEntity, partialTicks, ms, buffer, light, overlay)
 
         val connection = when (blockEntity.blockState.getValue(IHeatableBlock.GAS_HEAT_LEVEL)) {
-            GasHeatLevel.COOL -> ClockworkPartials.DUCT_CONN_COOL
-            GasHeatLevel.WARM -> ClockworkPartials.DUCT_WARM
-            GasHeatLevel.HOT -> ClockworkPartials.DUCT_HOT
+            GasHeatLevel.COOL -> ClockworkPartials.DUCT_CONN
+            GasHeatLevel.WARM -> ClockworkPartials.DUCT_CONN_WARM
+            GasHeatLevel.HOT -> ClockworkPartials.DUCT_CONN_HOT
+            GasHeatLevel.VERY_HOT -> ClockworkPartials.DUCT_CONN_VERY_HOT
+            GasHeatLevel.SUPER_HOT -> ClockworkPartials.DUCT_CONN_SUPER_HOT
+            GasHeatLevel.MOLTEN -> ClockworkPartials.DUCT_CONN_MOLTEN
+            null -> ClockworkPartials.DUCT_CONN
         }
 
     }
