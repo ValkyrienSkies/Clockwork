ba512cbed471d3df2a1df7a90f89230b11bb1a12
PriestOfFerns <timofeydenin@gmail.com>
2024-07-26 01:11:55 +0300

    fancy duct info widget

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/gas/IHeatableBlockEntity.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/gas/IHeatableBlockEntity.kt
@@ -1,7 +1,9 @@
 package org.valkyrienskies.clockwork.content.logistics.gas
 
 import com.simibubi.create.content.equipment.goggles.IHaveGoggleInformation
+import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
 import net.minecraft.network.chat.TextComponent
 import org.valkyrienskies.clockwork.ClockworkMod
 import org.valkyrienskies.clockwork.kelvin.api.DuctNodePos
@@ -9,6 +11,8 @@ import org.valkyrienskies.clockwork.kelvin.api.DuctNodePos
 interface IHeatableBlockEntity: IHaveGoggleInformation {
     fun getDuctNodePosition(): DuctNodePos
     override fun addToGoggleTooltip(tooltip: MutableList<Component>, isPlayerSneaking: Boolean): Boolean {
+        tooltip.add(TextComponent("    Duct Info").withStyle(ChatFormatting.GRAY))
+
         var found = false
         if (ClockworkMod.getKelvin().getTemperatureAt(this.getDuctNodePosition()) != 0.0) {
             tooltip.add(TextComponent("Temperature: ${ClockworkMod.getKelvin().getTemperatureAt(this.getDuctNodePosition())} K"))
