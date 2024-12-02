aac8729d5ff7b6c87c970a5285f24f4b00a30ded
PriestOfFerns <timofeydenin@gmail.com>
2024-12-02 22:20:04 +0300

    Heater facing direction

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/gas/heater/GasHeaterBlock.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/logistics/gas/heater/GasHeaterBlock.kt
@@ -1,27 +1,35 @@
 package org.valkyrienskies.clockwork.content.logistics.gas.heater
 
+import com.simibubi.create.content.kinetics.base.HorizontalKineticBlock
 import com.simibubi.create.content.processing.basin.BasinBlockEntity
 import com.simibubi.create.content.processing.burner.BlazeBurnerBlock.HEAT_LEVEL
 import com.simibubi.create.content.processing.burner.BlazeBurnerBlock.HeatLevel
 import com.simibubi.create.foundation.block.IBE
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import org.valkyrienskies.clockwork.ClockworkBlockEntities
 import org.valkyrienskies.clockwork.content.logistics.gas.INodeBlock
 
-class GasHeaterBlock(properties: Properties) : Block(properties), IBE<GasHeaterBlockEntity>, INodeBlock {
+class GasHeaterBlock(properties: Properties) : HorizontalDirectionalBlock(properties), IBE<GasHeaterBlockEntity>, INodeBlock {
 
     init {
-        registerDefaultState(defaultBlockState().setValue(HEAT_LEVEL, HeatLevel.NONE))
+        registerDefaultState(defaultBlockState().setValue(HEAT_LEVEL, HeatLevel.NONE).setValue(FACING, Direction.NORTH))
+
     }
 
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block?, BlockState?>) {
-        super.createBlockStateDefinition(builder)
         builder.add(HEAT_LEVEL)
+        builder.add(FACING)
+        super.createBlockStateDefinition(builder)
+
     }
 
     override fun getBlockEntityClass(): Class<GasHeaterBlockEntity> {
@@ -45,6 +53,20 @@ class GasHeaterBlock(properties: Properties) : Block(properties), IBE<GasHeaterB
     override fun onRemove(state: BlockState, level: Level, pos: BlockPos, newState: BlockState, isMoving: Boolean) {
         nodeRemove(state, level, pos, newState, isMoving)
         super.onRemove(state, level, pos, newState, isMoving)
+    }
+
+    override fun getStateForPlacement(context: BlockPlaceContext): BlockState {
+        return defaultBlockState()
+            .setValue(
+                HorizontalKineticBlock.HORIZONTAL_FACING, context.horizontalDirection
+                    .opposite
+            )
+    }
+
+    override fun canConnectTo(self: BlockPos, other: BlockPos, direction: Direction, level: BlockGetter): Boolean {
+        if (direction.axis != level.getBlockState(self).getValue(FACING).axis && direction != Direction.DOWN) return false
+
+        return super.canConnectTo(self, other, direction, level)
     }
 
 }
