4c4d51d74b2f40aa4de50c81d8a2919f919853e8
PriestOfFerns <timofeydenin@gmail.com>
2024-12-06 22:29:04 +0300

    perlin noise + random meta ball position

--- common/src/main/kotlin/org/valkyrienskies/clockwork/content/curiosities/asteroid/meteoroidGenerator.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/content/curiosities/asteroid/meteoroidGenerator.kt
@@ -7,12 +7,15 @@ import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.levelgen.RandomSource
+import net.minecraft.world.level.levelgen.SingleThreadedRandomSource
 import net.minecraft.world.level.levelgen.synth.PerlinNoise
 import org.joml.Vector3d
 import org.valkyrienskies.clockwork.ClockworkBlocks
 import org.valkyrienskies.mod.common.util.toJOMLD
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.pow
 import kotlin.random.Random
 import kotlin.random.nextInt
 
@@ -21,18 +24,22 @@ object meteoroidGenerator {
 
     fun generate(level: ServerLevel,center: BlockPos, threshold: Double, maxDistance: Int) {
 
-        val center_2 = center.offset(Random.nextInt(1..maxDistance),Random.nextInt(1..maxDistance),Random.nextInt(1..maxDistance))
+
+
+        val center_2 = center.offset(Random.nextInt(-maxDistance..maxDistance),Random.nextInt(-maxDistance..maxDistance),Random.nextInt(-maxDistance..maxDistance))
 
 
         val balls = mutableListOf<Vector3d>()
         balls.add(center_2.toJOMLD())
         balls.add(center.toJOMLD())
 
+        val noise = PerlinNoise.create(SingleThreadedRandomSource(level.seed), mutableListOf(1,1))
 
         for (x in min(center.x,center_2.x)-(2/threshold).toInt()..max(center.x,center_2.x) +(2/threshold).toInt()) {
             for (y in min(center.y,center_2.y)-(2/threshold).toInt()..max(center.y,center_2.y)+(2/threshold).toInt()) {
                 for (z in min(center.z,center_2.z)-(2/threshold).toInt()..max(center.z,center_2.z)+(2/threshold).toInt()) {
-                    if (metaBall(threshold, balls as List<Vector3d>, BlockPos(x,y,z).toJOMLD()))  {
+                    val t = threshold - noise.getValue(x.toDouble(),y.toDouble(),z.toDouble())/100
+                    if (metaBall(t, balls as List<Vector3d>, BlockPos(x,y,z).toJOMLD()))  {
                         level.setBlockAndUpdate(BlockPos(x,y,z), Blocks.STONE.defaultBlockState())
                     }
 
