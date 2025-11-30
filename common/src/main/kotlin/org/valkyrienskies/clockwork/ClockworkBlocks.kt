5f90df83bf51dc44961063a322eb29e604b58bc3
PriestOfFerns <timofeydenin@gmail.com>
2025-12-01 00:58:08 +0300

    make flap bearings get occluded

--- common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkBlocks.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkBlocks.kt
@@ -172,6 +172,7 @@ object ClockworkBlocks {
             .transform(axeOrPickaxe())
             .transform(ClockworkStress.setImpact(4.0))
             .addLayer { Supplier { RenderType.cutout() } }
+            .properties { it.noOcclusion() }
             .tag(AllTags.AllBlockTags.SAFE_NBT.tag)
             .item()
             .tab(ClockworkMod.BASE_CREATIVE_TABINFO)
@@ -187,6 +188,7 @@ object ClockworkBlocks {
             //.transform(flapbearing())
             .transform(ClockworkStress.setImpact(4.0))
             .addLayer { Supplier { RenderType.cutout() } }
+            .properties { it.noOcclusion() }
             .tag(AllTags.AllBlockTags.SAFE_NBT.tag)
             .item()
             .tab(ClockworkMod.BASE_CREATIVE_TABINFO)
