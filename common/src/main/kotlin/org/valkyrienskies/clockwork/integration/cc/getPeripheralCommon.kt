0faa49aa83da8a3eb7b7dd6cea042c52d333d678
spaceeye <superspaceeye@gmail.com>
2026-01-05 02:16:22 +0300

    * fabric cc compat

--- common/src/main/kotlin/org/valkyrienskies/clockwork/integration/cc/getPeripheralCommon.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/integration/cc/getPeripheralCommon.kt
@@ -3,11 +3,12 @@ package org.valkyrienskies.clockwork.integration.cc
 import dan200.computercraft.api.peripheral.IPeripheral
 import net.minecraft.core.Direction
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
+import org.valkyrienskies.clockwork.ClockworkBlockEntities
 import org.valkyrienskies.clockwork.content.contraptions.phys.bearing.PhysBearingBlockEntity
 
-fun getPeripheralCommon(be: BlockEntity, direction: Direction?): IPeripheral? {
-    return when(be) {
-        is PhysBearingBlockEntity -> PhysBearingPeripheral(be)
-        else -> null
-    }
-}
+val PERIPHERALS = mapOf<BlockEntityType<*>, (BlockEntity, Direction?) -> IPeripheral>(
+    ClockworkBlockEntities.PHYS_BEARING.get() to {be, _ -> PhysBearingPeripheral(be as PhysBearingBlockEntity)}
+)
+
+fun getPeripheralCommon(be: BlockEntity, direction: Direction?): IPeripheral? = PERIPHERALS[be.type]?.invoke(be, direction)
