c619516394e3375d53a7fb58464fbab2c0357f3c
blockninja124 <64211894+blockninja124@users.noreply.github.com.>
2026-08-12 21:34:20 -0400

    Fix import

--- common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkMod.kt
+++ common/src/main/kotlin/org/valkyrienskies/clockwork/ClockworkMod.kt
@@ -28,7 +28,6 @@ import org.valkyrienskies.clockwork.content.contraptions.flap.dual_link.DualLink
 import org.valkyrienskies.clockwork.content.events.CollisionSoundEffectHandler
 import org.valkyrienskies.clockwork.content.forces.*
 import org.valkyrienskies.clockwork.content.forces.contraption.BearingController
-import org.valkyrienskies.clockwork.content.forces.contraption.BearingController
 import org.valkyrienskies.clockwork.content.physicalities.gyro.GyroShipControl
 import org.valkyrienskies.clockwork.integration.cc.GenericPeripheralsCommon
 import org.valkyrienskies.clockwork.util.ClockworkUtils
